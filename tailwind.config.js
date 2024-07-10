a1f17e9ea2d01184e27ae125c7cdf27a9a042dde
rohanOO769 <aminrohan54@gmail.com>
2024-07-10 20:16:30 +0530

    set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{html,js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{html,js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
